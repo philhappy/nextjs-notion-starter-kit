be30d81e75f7dadeac0d8adbb5ec3edb7503234d
philhappy <77133643+philhappy@users.noreply.github.com>
2021-06-14 21:14:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ed236764797b4740b053948a0116393e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'see you down the road',
+  domain: 'https://www.notion.so/philhappy/ed236764797b4740b053948a0116393e',
+  author: 'phil',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,8 +17,8 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: 'phil94921145',
+  github: 'philhappy',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
